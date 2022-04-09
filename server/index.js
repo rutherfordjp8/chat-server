dcce7b9870181ba94461a973c4176b0f3b888869
Patrick <jpatrickrutherford@gmail.com>
2022-04-08 18:19:46 -0700

    created full stack socket io chat server

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "http://localhost:3001" } }); // dev port is 3001 in package.json script
+
+console.log("CURENT ENV", process.env.NODE_ENV, "port", process.env.PORT);
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
+// app.get("/rooms", (req, res) => {
+//   // console.log(typeof io.sockets.adapter.rooms);
+//   // res.status(200).send(JSON.stringify(io.sockets.adapter.rooms));
+//   const a = io.sockets.adapter.rooms.values();
+//   // console.log(a);
+//   res.json(Array.from(a));
+// });
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("message", (message, room = "") => {
+    console.log(message, room);
+    if (!room) socket.broadcast.emit("receive-message", message);
+    else socket.to(room).emit("receive-message", message);
+  });
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    socket.to(room).emit("joined-room", `${socket.id} joined room ${room}`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
---
+++ src/App.tsx
@@ -0,0 +1,198 @@
+import "./App.css";
+
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import io, { Socket } from "socket.io-client";
+
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
+import axios from "axios";
+import logo from "./logo.svg";
+
+const MESSAGE_STYLES = {
+  from: { alignSelf: "flex-end", bgcolor: "#1876d1" },
+  to: { alignSelf: "flex-start", bgcolor: "gray" },
+  JOINED_ROOM: { alignSelf: "center", textAlign: "center", bgcolor: "blue" },
+};
+
+type Message = {
+  message: string;
+  from?: string;
+  type: "from" | "to" | "JOINED_ROOM";
+};
+
+function App() {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [ws, setWS] = useState<Socket<
+    DefaultEventsMap,
+    DefaultEventsMap
+  > | null>(null);
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [currentRoom, setCurrentRoom] = useState("");
+  // const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const getRooms = async () => {
+      const allRooms = await axios.get("/rooms");
+      console.log(allRooms);
+      // setRooms(allRooms);
+    };
+
+    getRooms();
+
+    if (!ws) {
+      const socket = io("localhost:3000");
+      setWS(socket);
+    } else {
+      ws.removeAllListeners();
+      ws.on("receive-message", (messageObj) => {
+        console.log("got a message", messageObj);
+        const message = messageObj?.message;
+        const from = messageObj?.from;
+
+        setMessages([...messages, { message, from, type: "to" }]);
+      });
+
+      ws.on("joined-room", (message) => {
+        setMessages([...messages, { message, type: "JOINED_ROOM" }]);
+      });
+    }
+  }, [messages, setMessages, ws]);
+
+  const sendMessage = () => {
+    if (ws) {
+      console.log("sending message");
+      const newMessage: Message = { message, from: ws.id, type: "from" };
+
+      ws.emit("message", newMessage, currentRoom);
+      // messages.push({ message: message, from: ws.id });
+      // messages.push(newMessage);
+      // setMessages(messages);
+      setMessages([...messages, newMessage]);
+    } else {
+      alert("Not connected to websocket");
+    }
+  };
+
+  const joinRoom = () => {
+    if (ws) {
+      ws.emit("join-room", room);
+      setCurrentRoom(room);
+    }
+  };
+
+  // console.log("messages;", messages);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            borderRadius: ".5em",
+            overflow: "hidden",
+            height: "50vh",
+            minHeight: "20em",
+            width: "70%",
+            marginTop: "4em",
+            color: "white",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              bgcolor: "black",
+              height: "100%",
+              overflowY: "scroll",
+              padding: "1em",
+              color: "white",
+              textAlign: "left",
+            }}
+          >
+            {/* <img
+              style={{ position: "relative" }}
+              src={logo}
+              className="App-logo"
+              alt="logo"
+            /> */}
+            {messages.map(({ message, from, type }: Message) => {
+              return (
+                <Typography
+                  sx={{
+                    ...MESSAGE_STYLES[type],
+                    padding: "1em",
+                    borderRadius: "1em",
+                    margin: ".5em",
+                    overflow: "wrap",
+                    maxWidth: "80%",
+                    wordBreak: "break-word",
+                  }}
+                >
+                  {message}
+                </Typography>
+              );
+            })}
+          </Box>
+          <Box
+            sx={{
+              bgcolor: "white",
+              minWidth: "12em",
+              display: "flex",
+            }}
+          >
+            <TextField
+              onChange={(e) => setMessage(e.target.value)}
+              value={message}
+              fullWidth
+              label="Message"
+              variant={"filled"}
+              defaultValue="Message"
+            />
+            <Button
+              sx={{ width: "30%" }}
+              style={{ borderRadius: "0px" }}
+              onClick={sendMessage}
+              variant="contained"
+              color="primary"
+              disabled={!ws}
+            >
+              SEND
+            </Button>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            marginTop: "1em",
+            bgcolor: "white",
+            minWidth: "12em",
+            display: "flex",
+            borderRadius: "5px",
+          }}
+        >
+          <TextField
+            sx={{ borderRadius: "20px" }}
+            onChange={(e) => setRoom(e.target.value)}
+            value={room}
+            label="room"
+            fullWidth
+            variant={"filled"}
+            defaultValue="Message"
+          />
+          <Button
+            sx={{ width: "70%" }}
+            onClick={joinRoom}
+            variant="contained"
+            color="primary"
+            disabled={!ws}
+          >
+            Join Room
+          </Button>
+        </Box>
+      </header>
+    </div>
+  );
+}
+
+export default App;
